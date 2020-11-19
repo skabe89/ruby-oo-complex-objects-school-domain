dd9de9f562086e649b3137b1c2107fb9693b3265
Sean <skabe89@gmail.com>
2020-11-19 12:55:37 -0500

    commit tree 4e2ecb4cbbde2f84ff7137d67ed14a6b9fad346a

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+
+class School
+    attr_accessor :roster, :grade
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        @roster[grade] == nil ? @roster[grade] = [student] : @roster[grade] << student
+    end
+
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.map{|grade, student| student.sort!}
+        @roster
+    end
+
+end
